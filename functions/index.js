71b8441705f68f6a99151a3b62689d68f84a0df8
luis sanchez <sanchezl391@gmail.com>
2019-03-03 16:34:54 -0700

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send({hello: 'there'});
+});
+
+exports.app = functions.https.onRequest(app);
